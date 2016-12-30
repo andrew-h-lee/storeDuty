f050485621e5ce2cae6290befa3d5fa19a651980
dark419 <mr.ahlee@gmail.com>
2016-12-31 00:02:03 +1100

    Add Order Folder Component

---
+++ src/components/orders/orders_table.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class OrdersTable extends React.Component {
+  render() {
+    return (
+      <table className='orders-table'>
+        <thead>
+          <tr>
+            <th>Order #</th>
+            <th>Customer</th>
+            <th className='sorted-by'>Ordered at</th>
+            <th>Product(s)</th>
+            <th className='amount'>Amount</th>
+            <th className='status'>Payment status</th>
+            <th className='status'>Order status</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  }
+}
+
+export default OrdersTable;
